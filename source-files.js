84cfcceddd2f2ee574162b8883ebfa39dfd99a4c
chrislearn <chris@acroidea.com>
2021-01-11 14:02:06 +0000

    Deploying to gh-pages from  @ 1c94d7acaedfb19ffaef6393ea1bb66e66bd6e07 🚀

--- source-files.js
+++ source-files.js
@@ -2,5 +2,4 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["salvo"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["salvo_core"] = {"name":"","dirs":[{"name":"http","dirs":[{"name":"errors","files":["http_error.rs","mod.rs","read_error.rs"]},{"name":"header","files":["mod.rs"]},{"name":"multipart","dirs":[{"name":"field","files":["headers.rs","mod.rs"]}],"files":["boundary.rs","helpers.rs","mod.rs"]}],"files":["body_chunk.rs","form.rs","mod.rs","range.rs","request.rs","response.rs"]},{"name":"routing","dirs":[{"name":"filter","dirs":[{"name":"impls","files":["method.rs","mod.rs","path.rs"]}],"files":["and.rs","and_then.rs","mod.rs","or.rs","or_else.rs"]}],"files":["mod.rs","router.rs"]},{"name":"writer","dirs":[{"name":"file","files":["mod.rs","named_file.rs"]}],"files":["mod.rs"]}],"files":["catcher.rs","depot.rs","handler.rs","lib.rs","pick_port.rs","server.rs"]};
 sourcesIndex["salvo_extra"] = {"name":"","dirs":[{"name":"auth","files":["basic.rs","error.rs","jwt.rs","mod.rs"]},{"name":"serve","files":["mod.rs"]}],"files":["lib.rs"]};
-sourcesIndex["salvo_macro"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
